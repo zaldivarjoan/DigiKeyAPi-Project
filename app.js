ab9fed4f868f8aa0b82355b531e50bd9805a3277
Dat Nguyen <tomnguyen2111996@gmail.com>
2024-03-27 10:32:22 -0700

    dat

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import searchByKeyword from './api.js';
 import { create, find } from './db.js';
+import fs from 'fs';
 
-// Function to handle searching by keyword
+//Function to handle searching by keyword
 const handleKeywordSearch = async (keyword, useCache = false) => {
   try {
     // Perform search based on the keyword
@@ -36,6 +37,51 @@ const handleKeywordSearch = async (keyword, useCache = false) => {
   }
 };
 
+//       //Getting the search results from API based on keyword
+//       const searchResults = await searchByKeyword(keyword);
+
+//       //Saving the results to the search_history file
+//       let resultCount = searchResults.Products.length;
+//       const searchData = {
+//           search: keyword,
+//           resultCount: resultCount 
+//       };
+      
+//       const previousSearches = await fs.readFile('./mock_database/search_history.json', {encoding: 'utf-8'}, (err) =>{
+//           if(err){
+//               console.error('Error when reading file: ', err);
+//           }
+//       });
+//       const previousSearchesArray = previousSearches ? JSON.parse(previousSearches) : [];
+//       console.log(`The array after reading file:\n ${previousSearchesArray}`);
+//       //Incase the search is already logged in the history
+//       const duplicateIndex = previousSearchesArray.findIndex(current => current.search === searchData.search && current.resultCount === searchData.resultCount);
+//       console.log(`If duplicateIndex = -1: NO duplicate \n If duplicateIndex > -1: Yes duplicate \n ${duplicateIndex}`)
+//       if(duplicateIndex < 0){
+//           previousSearchesArray.push(searchData);
+//           console.log(`The array after pushing to array:\n ${previousSearchesArray}`);
+//           await fs.writeFile('./mock_database/search_history.json', JSON.stringify(previousSearchesArray, null, 2), (err) =>{
+//               if(err){
+//                   console.error('Error when writing to file: ', err);
+//               }
+//           });
+//       }
+
+//       console.log(`The new search object: ${searchData.search} & ${searchData.resultCount}`);
+
+//       //Displaying the results to the user
+//       console.log(`Result list for: ${keyword}`);
+//       for(let i = 0; i < searchResults.Products.length; i++){
+//           console.log(`${i + 1}. ${searchResults.Products[i].ManufacturerProductNumber}`)
+//       }
+
+//   } catch (error) {
+//       console.error('Error handling keyword search:', error);
+//   }
+
+// }
+
+
 // Function to handle displaying search history
 const displaySearchHistory = async () => {
   try {
@@ -52,7 +98,7 @@ const displaySearchHistory = async () => {
 const saveToSearchHistory = async (keyword, searchResults) => {
   try {
     // Create a new entry in the search history with the keyword and result count
-    await create('./search_history', { search: keyword, resultCount: searchResults.length });
+    await create('./search_history', { search: keyword, resultCount: searchResults.Products.length });
   } catch (error) {
     console.error('Error saving search history:', error.message);
   }
@@ -84,6 +130,6 @@ const promptUserToSelectItem = async (searchResults) => {
 
 //export { searchByKeywordApp, displaySearchHistory };
 
-handleKeywordSearch('IVIEW 14" Laptop');
+handleKeywordSearch('History');
 
 
